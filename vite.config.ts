5c14c760da179764cb82952f03c6050b007ed65a
tigraaleks-netizen <tigraaleks@gmail.com>
2026-03-30 01:14:37 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig(({ mode }) => {
+  base: './',
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
