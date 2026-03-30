86a7991255c258f2733d816d22a2978cb0a5d2c4
tigraaleks-netizen <tigraaleks@gmail.com>
2026-03-30 14:15:05 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: './',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    emptyOutDir: true,
-  },
+plugins: [react()],
+base: '',
+build: {
+outDir: 'dist',
+assetsDir: 'assets',
+emptyOutDir: true,
+},
 })
